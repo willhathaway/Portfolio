27cc25fbdf199e2bb874d8dbf3fb13f08828bbfa
Will Hathaway <william.hathaway38@gmail.com>
2020-01-23 11:27:20 -0500

    wildway animation done

---
+++ views/javascript/animation.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    console.log("hello");
+
+    let name = "william daniel hathaway";
+
+    let nameArray = name.toUpperCase().split('');
+
+    for (let i = 0; i < 23; i++) {
+        let y = i+1;
+        if (y == 1 || y == 2 || y == 3 ||y == 9 || y == 21 || y == 22 || y == 23) {
+            $(".animate").append(this["div"+(i+1)] = $("<div class='ww letterDiv' id = 'div" + (i+1) + "' >"));
+        } else {
+            $(".animate").append(this["div"+(i+1)] = $("<div class='nw letterDiv' id = 'div" + (i+1) + "' >"));
+        }
+    }
+
+    for (let i = 0; i < nameArray.length; i++) {
+        $('#div' + (i+1)).html(nameArray[i]);
+    }
+    
+
+})

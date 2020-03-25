1533b6a345b4afbd4fc83ca8a617dc9dfec0d5dc
Will Hathaway <william.hathaway38@gmail.com>
2020-03-25 00:15:20 -0400

    changes made for employer ready status: github links and email link in contact page

--- views/javascript/animation.js
+++ views/javascript/animation.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
 
+    // console log that the animation file was successfully loaded:
     console.log("animation loaded");
 
+    // name defined, used to create the letter divs in the for loop below:
     let name = "william daniel hathaway";
 
+    // name variable split into an array:
     let nameArray = name.toUpperCase().split('');
 
+    // loop through the array to create a letter div and push that newly created div to the .animate div:
     for (let i = 0; i < 23; i++) {
         let y = i+1;
         if (y == 1 || y == 2 || y == 3 ||y == 9 || y == 21 || y == 22 || y == 23) {
